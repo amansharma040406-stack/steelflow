139d0da2569d976cc16de6b6e114533e7ee75538
aman demo <amansharma040406@gmail.com>
2026-09-12 18:15:57 +0530

    readme and camera.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from backend.app.models.camera import Camera 
 app=FastAPI()
-@app.get("/")
+@app.get("/health")
 def home():
-    return{"message":"steeelflow api is running"}git remote add origin https://github.com/amansharma040406-stack/steelflow.git
-git branch -M main
-git push -u origin main
+    return{"message":"steeelflow api is running"}
+@app.get("/cameras")
+def get_camera():
+    camera1=Camera(
+        1,
+        "camera 1",
+        "gate A",
+        True
+    )
+    camera2=Camera(
+        2,
+        "camera 2",
+        "gate B",
+        True
+    )
+    return[
+        {
+            "id":camera1.id,
+            "name":camera1.name,
+            "location":camera1.location,
+            "is_active":camera1.is_active
+        },
+        {
+            "id":camera2.id,
+            "name":camera2.name,
+            "location":camera2.location,
+            "is_active":camera2.is_active
+        }
+    ]
---
+++ backend/app/models/camera.py
@@ -0,0 +1,6 @@
+class Camera:
+    def __init__(self, id, name, location, is_active):
+        self.id = id
+        self.name = name
+        self.location = location
+        self.is_active = is_active
