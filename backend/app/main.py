b73cdd1fb2216f3cf6984259b1c63812ddfe2d7f
aman demo <amansharma040406@gmail.com>
2026-09-22 23:18:28 +0530

    complete camera CRUD

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,38 +1,47 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from .models.camera import Camera
 from .database import SessionLocal, engine, Base
+from .models.camera import Camera
 from .schemas.camera import CameraCreate, CameraResponse, CameraUpdate
-from fastapi import FastAPI, Depends, HTTPException
 
+
+# Create database tables
 Base.metadata.create_all(bind=engine)
 
-app=FastAPI()
+app = FastAPI()
+
+# Health Check
+
 @app.get("/health")
 def home():
-    return{"message":"steeelflow api is running"}
+    return {"message": "steelflow api is running"}
+
+# Database Dependency
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
+
     try:
         yield db
     finally:
         db.close()
+
+
+# GET ALL CAMERAS
 @app.get("/cameras", response_model=list[CameraResponse])
 def get_camera(db: Session = Depends(get_db)):
-    cameras=db.query(Camera).all()
-    result=[]
-    for camera in cameras:
-        result.append({
-            "id":camera.id,
-            "camera_uid":camera.camera_uid,
-            "name":camera.name,
-            "location":camera.location,
-            "is_active":camera.is_active
-        })
-    return result
-@app.post("/cameras")
-def create_camera(camera: CameraCreate, db: Session = Depends(get_db)):
+
+    cameras = db.query(Camera).all()
+
+    return cameras
+
+# CREATE CAMERA
+@app.post("/cameras", response_model=CameraResponse)
+def create_camera(
+    camera: CameraCreate,
+    db: Session = Depends(get_db)
+):
+
     new_camera = Camera(
         camera_uid=camera.camera_uid,
         name=camera.name,
@@ -44,59 +53,71 @@ def create_camera(camera: CameraCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_camera)
 
-    return {
-        "id": new_camera.id,
-        "camera_uid": new_camera.camera_uid,
-        "name": new_camera.name,
-        "location": new_camera.location,
-        "is_active": new_camera.is_active
-    }
+    return new_camera
 
-@app.post("/cameras")
-def create_camera(camera: CameraCreate, db: Session = Depends(get_db)):
 
-    new_camera = Camera(
-        name=camera.name,
-        location=camera.location,
-        is_active=camera.is_active
-    )
-
-    db.add(new_camera)
-    db.commit()
-    db.refresh(new_camera)
-
-    return {
-        "id": new_camera.id,
-        "name": new_camera.name,
-        "location": new_camera.location,
-        "is_active": new_camera.is_active
-    }
+# GET CAMERA BY ID
 @app.get("/cameras/{camera_id}", response_model=CameraResponse)
-def get_camera_by_id(camera_id: int, db: Session = Depends(get_db)):
+def get_camera_by_id(
+    camera_id: int,
+    db: Session = Depends(get_db)
+):
 
     camera = db.query(Camera).filter(Camera.id == camera_id).first()
 
     if camera is None:
-        raise HTTPException(status_code=404, detail="Camera not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Camera not found"
+        )
 
     return camera
 
-@app.put("/camera/{camera_id}", response_model=CameraResponse)
-def update_camera(
-    camera_id:int,
-    camera_data:CameraUpdate,
-    db:Session=Depends(get_db)
 
+# UPDATE CAMERA
+@app.put("/cameras/{camera_id}", response_model=CameraResponse)
+def update_camera(
+    camera_id: int,
+    camera_data: CameraUpdate,
+    db: Session = Depends(get_db)
 ):
+
     camera = db.query(Camera).filter(Camera.id == camera_id).first()
+
     if camera is None:
-            raise HTTPException(status_code=404, detail="Camera not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Camera not found"
+        )
+
     camera.name = camera_data.name
-    camera.location=camera_data.location
-    camera.is_active=camera_data.is_active
+    camera.location = camera_data.location
+    camera.is_active = camera_data.is_active
 
     db.commit()
     db.refresh(camera)
 
     return camera
 
+#delete camera:
+@app.delete("/cameras/{camera_id}")
+def delete_camera(
+    camera_id: int,
+    db: Session = Depends(get_db)
+):
+    camera = db.query(Camera).filter(Camera.id == camera_id).first()
+
+    if camera is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Camera not found"
+        )
+
+    db.delete(camera)
+    db.commit()
+
+    return {
+        "message": "Camera deleted successfully",
+        "camera_id": camera_id
+    }
+    
